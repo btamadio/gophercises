62417d0aa95cc53cd4275797c3ab496b4776181f
Brian Amadio <btamadio@Brians-MBP.attlocal.net>
2020-03-01 19:36:32 -0800

    start on choose your own adventure

--- adventure/main.go
+++ adventure/main.go
@@ -5,25 +5,40 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 )
 
-type StoryArc struct{
-	Title string `json:"title"`
+type StoryArc struct {
+	Title      string   `json:"title"`
 	Paragraphs []string `json:"story"`
-	Options []struct{
-		Text string `json:"text"`
+	Options    []struct {
+		Text    string `json:"text"`
 		ArcName string `json:"arc"`
 	} `json:"options"`
 }
 
-func main(){
+func arcHandler(s StoryArc) http.HandlerFunc {
+
+	// TODO: Use HTML templates to display the title, paragraphs, and options, with links to other pages
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, fmt.Sprintf("%+v", s))
+	}
+}
+
+func main() {
 	var book map[string]StoryArc
 	jsonData, err := ioutil.ReadFile("gopher.json")
-	if err != nil{
+	if err != nil {
 		log.Fatal("failed to open json file")
 	}
-	if err := json.Unmarshal(jsonData, &book); err != nil{
+	if err := json.Unmarshal(jsonData, &book); err != nil {
 		log.Fatal("failed to unmarshal json")
 	}
-	fmt.Printf("%+v\n", book)
+
+	for arcName := range book {
+		http.HandleFunc("/"+arcName, arcHandler(book[arcName]))
+	}
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
